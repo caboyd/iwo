85b48d5d3034fc92f9777b82ef51ad0ee6da3c8b
Chris <boyd111c@gmail.com>
2018-07-20 21:59:05 -0600

    Added Camera that uses quaternion for orientation.

---
+++ src/cameras/Camera.ts
@@ -0,0 +1,150 @@
+import { glMatrix, mat3, mat4, quat, vec3 } from "gl-matrix";
+
+const SPEED: number = 200.0;
+const SENSITIVITY: number = 0.005;
+
+export enum Camera_Movement {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT
+}
+
+let forward = vec3.create();
+let right = vec3.create();
+let temp_quat = quat.create();
+
+let FORWARD = vec3.fromValues(0,0,-1);
+
+export class Camera {
+    public position: vec3;
+    public front: vec3;
+    public up: vec3;
+
+    private right: vec3;
+    private worldUp: vec3;
+
+    private movementSpeed: number;
+    private mouseSensitivity: number;
+
+    private pitch: number;
+    private heading: number;
+
+    private orientation: quat;
+
+    constructor(pos: vec3, forward: vec3 = FORWARD, up: vec3 = vec3.fromValues(0, 1, 0)) {
+        this.position = vec3.clone(pos);
+        this.front = vec3.clone(forward);
+        vec3.normalize(this.front,this.front);
+        
+        this.worldUp = vec3.fromValues(0, 1, 0);
+        this.right = vec3.cross(vec3.create(), this.front, this.worldUp);
+        vec3.normalize(this.right,this.right);
+        this.up = vec3.clone(up);
+
+        vec3.normalize(this.up,this.up);
+        
+        console.log(this.front);
+        console.log(this.right);
+        console.log(this.up);
+        
+        this.movementSpeed = SPEED;
+        this.mouseSensitivity = SENSITIVITY;
+       
+        this.pitch =  Math.asin(-this.front[1]);
+        this.heading = -Math.atan2(this.front[0], this.front[2]) - Math.PI;
+    
+        this.orientation = quat.create();
+        this.calculateOrientation();
+    }
+
+    public getRight(out: vec3): vec3 {
+        quat.conjugate(temp_quat,this.orientation);
+        vec3.set(out,1,0,0);
+        vec3.transformQuat(out, out, temp_quat);
+        return out;
+    }
+    
+    public getForward(out:vec3):vec3{
+        quat.conjugate(temp_quat,this.orientation);
+        vec3.set(out,0,0,-1);
+        vec3.transformQuat(out, out, temp_quat);
+        return out;
+    }
+
+    public lookAt(target: vec3): void {
+        vec3.sub(this.front,target,this.position);
+        vec3.normalize(this.front,this.front);
+       
+         this.pitch = -Math.asin(this.front[1]);
+         this.heading = -Math.atan2(this.front[0], this.front[2]) - Math.PI;
+            
+         console.log(this.front);
+         console.log(this.heading);
+         this.calculateOrientation();
+    }
+
+    public getViewMatrix(out: mat4): mat4 {
+        mat4.fromQuat(out, this.orientation);
+        mat4.translate(out, out, vec3.negate(vec3.create(), this.position));
+        return out;
+    }
+
+    public getInverseViewMatrix(out: mat4): mat4 {
+        out = this.getViewMatrix(out);
+        mat4.invert(out, out);
+        return out;
+    }
+
+    public processKeyboard(direction: Camera_Movement, deltaTime: number): void {
+        let velocity: number = this.movementSpeed * deltaTime;
+
+        forward = this.getForward(forward);
+        right = this.getRight(right);
+
+        if (direction == Camera_Movement.FORWARD) {
+            vec3.scale(forward, forward, velocity);
+            vec3.add(this.position, this.position, forward);
+        } else if (direction == Camera_Movement.BACKWARD) {
+            vec3.scale(forward, forward, -velocity);
+            vec3.add(this.position, this.position, forward);
+        } else if (direction == Camera_Movement.LEFT) {
+            vec3.scale(right, right, -velocity);
+            vec3.add(this.position, this.position, right);
+        } else if (direction == Camera_Movement.RIGHT) {
+            vec3.scale(right, right, velocity);
+            vec3.add(this.position, this.position, right);
+        }
+    }
+
+    public processMouseMovement(xOffset: number, yOffset: number, constrainPitch: boolean = true): void {
+        if (xOffset === 0 && yOffset === 0) return;
+
+        xOffset *= this.mouseSensitivity;
+        yOffset *= this.mouseSensitivity;
+
+        this.heading += xOffset;
+        if (this.heading > 2 * Math.PI) this.heading -= 2 * Math.PI;
+        if (this.heading < 0) this.heading += 2 * Math.PI;
+
+        this.pitch += yOffset;
+        if (this.pitch > Math.PI) this.pitch -= 2 * Math.PI;
+        if (this.pitch < -Math.PI) this.pitch += 2 * Math.PI;
+
+        if (constrainPitch) {
+            if (this.pitch > Math.PI / 2) this.pitch = Math.PI / 2;
+            if (this.pitch < -Math.PI / 2) this.pitch = -Math.PI / 2;
+        }
+        
+        this.calculateOrientation();
+    }
+
+    private calculateOrientation() {
+        let pitch_quat = quat.setAxisAngle(quat.create(), this.right, this.pitch);
+        let heading_qat = quat.setAxisAngle(quat.create(), this.worldUp, this.heading);
+
+        quat.identity(this.orientation);
+        quat.mul(this.orientation, this.orientation, pitch_quat);
+        quat.mul(this.orientation, this.orientation, heading_qat);
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-import {glMatrix, mat3, mat4, vec3} from "gl-matrix";
-import {BoxGeometry} from "src/geometry/BoxGeometry";
-import {Mesh} from "src/meshes/Mesh";
-import {MeshInstance} from "src/meshes/MeshInstance";
-import {Material} from "src/materials/Material";
-import {Renderer} from "src/graphics/Renderer";
-import {FileLoader} from "./loader/FileLoader";
-import {TextureLoader} from "./loader/TextureLoader";
-import {SphereGeometry} from "./geometry/SphereGeometry";
+import { glMatrix, mat3, mat4, vec3 } from "gl-matrix";
+import { BoxGeometry } from "src/geometry/BoxGeometry";
+import { Mesh } from "src/meshes/Mesh";
+import { MeshInstance } from "src/meshes/MeshInstance";
+import { Material } from "src/materials/Material";
+import { Renderer } from "src/graphics/Renderer";
+import { FileLoader } from "./loader/FileLoader";
+import { TextureLoader } from "./loader/TextureLoader";
+import { SphereGeometry } from "./geometry/SphereGeometry";
+import { Camera, Camera_Movement } from "./cameras/Camera";
 
 let canvas: HTMLCanvasElement;
 
@@ -16,9 +17,15 @@ let view_matrix: mat4 = mat4.create();
 let view_matrix3x3: mat3 = mat3.create();
 let proj_matrix: mat4 = mat4.create();
 
-let cPos: vec3 = vec3.fromValues(0, 0, 6.0);
+let cPos: vec3 = vec3.fromValues(0, 0,6.0);
 let cUp: vec3 = vec3.fromValues(0, 1, 0);
 let cFront: vec3 = vec3.fromValues(0, 0, -1);
+
+let camera: Camera;
+
+let mouse_x_total = 0;
+let mouse_y_total = 0;
+let keys: Array<boolean> = [];
 
 let mesh: Mesh;
 let box: MeshInstance;
@@ -31,6 +38,17 @@ let loading_subtext = document.getElementById("loading-subtext")!;
 let files_completed: number = 0;
 let total_files: number = 0;
 
+const moveCallback = (e: MouseEvent): void => {
+    //@ts-ignore
+    let movementX = e.movementX || e.mozMovementX || e.webkitMovementX || 0;
+    //@ts-ignore
+    let movementY = e.movementY || e.mozMovementY || e.webkitMovementY || 0;
+    if (e.which == 1) {
+        mouse_x_total += movementX;
+        mouse_y_total += movementY;
+    }
+};
+
 (function loadWebGL(): void {
     let global_root = window.location.href.substr(0, window.location.href.lastIndexOf("/"));
     let a = require("assets/container.jpg");
@@ -40,8 +58,10 @@ let total_files: number = 0;
     FileLoader.setOnFileComplete(onFileComplete);
 
     canvas = <HTMLCanvasElement>document.getElementById("canvas");
+    document.addEventListener("mousemove", moveCallback, false);
 
     gl = initGL();
+    camera = new Camera(cPos, cFront, cUp);
 
     let sphere_g = new SphereGeometry(0.7, 16, 16);
     let box_g = new BoxGeometry(2.0, 1.0, 1, 2, 1, 1, false);
@@ -53,7 +73,7 @@ let total_files: number = 0;
     let mat2 = new Material();
     mat2.albedo = TextureLoader.load(gl, b.src, global_root);
     total_files = 2;
-    
+
     box = new MeshInstance(mesh, mat);
     mat4.translate(box.model_matrix, box.model_matrix, vec3.fromValues(0, 3, 0));
     sphere = new MeshInstance(mesh2, mat2);
@@ -71,7 +91,7 @@ let total_files: number = 0;
 
     loading_text.innerText = files_completed + "/" + total_files + " files completed.";
 
-    requestAnimationFrame(drawScene);
+    requestAnimationFrame(update);
 })();
 
 function initGL(): WebGL2RenderingContext {
@@ -87,13 +107,29 @@ function initGL(): WebGL2RenderingContext {
     return gl;
 }
 
+function update(): void {
+    if (keys[87]) camera.processKeyboard(Camera_Movement.FORWARD, 0.001);
+    else if (keys[83]) camera.processKeyboard(Camera_Movement.BACKWARD, 0.001);
+    if (keys[65]) camera.processKeyboard(Camera_Movement.LEFT, 0.001);
+    else if (keys[68]) camera.processKeyboard(Camera_Movement.RIGHT, 0.001);
+    
+    if (keys[82]) camera.lookAt(vec3.fromValues(0, 0, -1));
+
+    camera.processMouseMovement(-mouse_x_total, -mouse_y_total, true);
+    mouse_x_total = 0;
+    mouse_y_total = 0;
+
+    drawScene();
+    requestAnimationFrame(update);
+}
+
 function drawScene(): void {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    mat4.lookAt(view_matrix, cPos, cFront, cUp);
+    camera.getViewMatrix(view_matrix);
     mat3.fromMat4(view_matrix3x3, view_matrix);
 
-    Renderer.PBRShader.setVec3ByName("u_eye_pos", cPos);
+    Renderer.PBRShader.setVec3ByName("u_eye_pos", camera.position);
 
     //mat4.identity(model_matrix);
 
@@ -101,30 +137,25 @@ function drawScene(): void {
     mat4.rotateZ(box.model_matrix, box.model_matrix, glMatrix.toRadian(0.5));
     mat3.normalFromMat4(box.normal_matrix, box.model_matrix);
 
-    box.render(gl, renderer,view_matrix,proj_matrix);
+    box.render(gl, renderer, view_matrix, proj_matrix);
 
-    let model =  sphere.model_matrix;
+    let model = sphere.model_matrix;
     let normal = sphere.normal_matrix;
 
     for (let i = 0; i < 11; i++) {
-        for(let k = 0; k < 11; k++){
+        for (let k = 0; k < 11; k++) {
             let j = i - 5;
             let w = k - 5;
             mat4.identity(model);
-            mat4.translate(model, model, vec3.fromValues(j * 2, w * 2, -2 + (j&1) +(k&1) ));
+            mat4.translate(model, model, vec3.fromValues(j * 2, w * 2, -2 + (j & 1) + (k & 1)));
 
             mat4.rotateY(model, model, glMatrix.toRadian(Date.now() * -0.08));
             //mat4.rotateZ(model, model, glMatrix.toRadian(Date.now() * 0.06));
 
             mat3.normalFromMat4(normal, model);
-
-            sphere.render(gl, renderer,view_matrix,proj_matrix);
+            sphere.render(gl, renderer, view_matrix, proj_matrix);
         }
-        
-
     }
-
-    requestAnimationFrame(drawScene);
 }
 
 function onProgress(loaded_bytes: number, total_bytes: number, file_name: string) {
@@ -148,3 +179,11 @@ function onFileComplete(file_name: string) {
         a.remove();
     }
 }
+
+window.onkeydown = function(e) {
+    keys[e.keyCode] = true;
+};
+
+window.onkeyup = function(e) {
+    keys[e.keyCode] = false;
+};
