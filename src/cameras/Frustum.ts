3790cd7d765c4c036827dc89df06c7e0282073cc
Chris <boyd111c@gmail.com>
2022-11-12 23:09:37 -0600

    add param for frustum corner function

--- src/cameras/Frustum.ts
+++ src/cameras/Frustum.ts
@@ -15,7 +15,7 @@ export const DefaultFrustumOptions = {
 export class Frustum {
     private opt: FrustumOptions;
 
-    private readonly inverse_view_matrix: mat4;
+    private readonly view_matrix: mat4;
 
     private far_height!: number;
     private far_width!: number;
@@ -23,12 +23,16 @@ export class Frustum {
     private near_width!: number;
 
     constructor(gl: WebGL2RenderingContext, inverse_view_matrix: mat4, options?: Partial<FrustumOptions>) {
-        this.inverse_view_matrix = mat4.clone(inverse_view_matrix);
+        this.view_matrix = mat4.clone(inverse_view_matrix);
         this.opt = { ...DefaultFrustumOptions, ...options };
         this.calculateWidthsAndHeights(gl);
     }
 
-    public calculateFrustumCorners(): number[][] {
+    /**
+     * Return the corners of the frustum first generated in camera space then multiplied by supplied spacial_transform_matrix
+     * @param {mat4} spacial_transform_matrix - The matrix space you want the corners to be return in. ex. light_view_matrix for lighspace or inverse_view_matrix for world space
+     */
+    public calculateFrustumCorners(spacial_transform_matrix: mat4): number[][] {
         const far_top_left = [-this.far_width / 2, this.far_height / 2, -this.opt.clip_far];
         const far_top_right = [this.far_width / 2, this.far_height / 2, -this.opt.clip_far];
         const far_bottom_left = [-this.far_width / 2, -this.far_height / 2, -this.opt.clip_far];
@@ -52,7 +56,7 @@ export class Frustum {
         for (const point of points) {
             //move point to camera space
             let point4f: vec4 = vec4.fromValues(point[0], point[1], point[2], 1.0);
-            vec4.transformMat4(point4f, point4f, this.inverse_view_matrix);
+            vec4.transformMat4(point4f, point4f, spacial_transform_matrix);
 
             //perspetive divite w component
             point[0] = point4f[0] / point4f[3];
