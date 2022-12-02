0ffbed1e08b4875ceb3835f8a502ed201da2eab9
Chris <boyd111c@gmail.com>
2022-12-02 17:15:28 -0600

    fix missing reset on index draw count

--- src/graphics/renderer/RendererStats.ts
+++ src/graphics/renderer/RendererStats.ts
@@ -19,6 +19,7 @@ export class RendererStats {
         this.index_buffer_bind_count = 0;
         this.vertex_buffer_bind_count = 0;
         this.vertex_draw_count = 0;
+        this.index_draw_count = 0;
         this.draw_calls = 0;
     }
 }
